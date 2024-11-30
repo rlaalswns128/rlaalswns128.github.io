77a16e5046731fa383670b4772390546f64988f6
rlaalswns128 <rlaalswns128@gmail.com>
2024-11-30 23:50:46 +0900

    Update project with new features

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/navbar.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
+      setIsMenuOpen(false);
     }
   };
 
   return (
-    <nav>
+    <nav className={isMenuOpen ? 'mobile-menu-open' : ''}>
       <div onClick={() => scrollToSection('home')} className="logo">MINJUNE</div>
       <div>
-      <a onClick={() => scrollToSection('profile')}>Profile</a>
+        <a onClick={() => scrollToSection('profile')}>Profile</a>
         <a onClick={() => scrollToSection('skills')}>Skills</a>
         <a onClick={() => scrollToSection('projects')}>Projects</a>
         <a onClick={() => scrollToSection('info')}>Info</a>
+      </div>
+      <div className="mobile-menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        ☰
       </div>
     </nav>
   );
