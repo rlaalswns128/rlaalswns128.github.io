a476af3d1cb1cc099bdbb17d2f931b0786ef9593
rlaalswns128 <rlaalswns128@gmail.com>
2024-11-30 23:40:03 +0900

    Update project with new features

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../styles/projects.css';
 
 const projects = [
-  { title: 'Portfolio Project - 2024', description: '인하공전 1학년 교과과정 정리', link: 'https://https://rlaalswns128.github.io/portfolio' },
+  { title: 'Portfolio Project - 2024', description: '개인 포트폴리오 프로젝트', link: 'https://github.com/rlaalswns128/rlaalswns128.github.io/tree/master' },
   { title: 'Capstone Design Project - 2024', description: 'Capstone Design Project - 2024', link: 'https://github.com/rlaalswns128/Uni/tree/b92462210954f7d27d92765651a23038c7f4ed8d/Projects/HealthCare' },
   { title: 'Software Project - 2024', description: 'Software Project - 2024', link: 'https://github.com/rlaalswns128/Uni/tree/de241c6366ea8b8eba8f4702f00790329f3ce6b5/Projects/WhiskyPedia' },
   { title: 'Software Project - 2023', description: 'Software Project - 2023', link: 'https://github.com/rlaalswns128/Uni/tree/de241c6366ea8b8eba8f4702f00790329f3ce6b5/Projects/QUESTorm' },
