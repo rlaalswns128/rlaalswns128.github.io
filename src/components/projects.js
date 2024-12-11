83631adef40d97f4fbffb1346ed3934abe618357
MinJune Kim <rlaalswns128@gmail.com>
2024-12-12 01:46:35 +0900

    update

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,7 +6,7 @@ const projects = [
   { title: 'Capstone Design Project - 2024', description: 'Capstone Design Project - 2024', link: 'https://github.com/rlaalswns128/University/tree/4cfa7739516753c30185749d5b9acda7a49a2dd7/Pill%EC%9A%94%ED%95%A8(Pill-YoHam)' },
   { title: 'Software Project - 2024', description: 'Software Project - 2024', link: 'https://github.com/rlaalswns128/University/tree/4cfa7739516753c30185749d5b9acda7a49a2dd7/WhiskyPedia' },
   { title: 'Software Project - 2023', description: 'Software Project - 2023', link: 'https://github.com/rlaalswns128/University/tree/4cfa7739516753c30185749d5b9acda7a49a2dd7/QUESTorm' },
-  { title: '스프링 프로젝트 - 2023', description: 'School Forum', link: 'https://github.com/rlaalswns128/University/tree/4cfa7739516753c30185749d5b9acda7a49a2dd7/School%20Forum' },
+  { title: '스프링 프로젝트 - 2023', description: '학교 게시판 만들기', link: 'https://github.com/rlaalswns128/University/tree/4cfa7739516753c30185749d5b9acda7a49a2dd7/School%20Forum' },
   { title: 'JSP 프로젝트 - 2022', description: 'Red Devils', link: 'https://github.com/rlaalswns128/University/tree/4cfa7739516753c30185749d5b9acda7a49a2dd7/RedDevils/src/main' },
   { title: '인하공전 3학년 교과과정 정리 - 2024', description: '인하공전 3학년 교과과정 정리', link: 'https://https://rlaalswns128.github.io/' },
   { title: '인하공전 2학년 교과과정 정리 - 2022', description: '인하공전 2학년 교과과정 정리', link: 'https://https://rlaalswns128.github.io/' },
